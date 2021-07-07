475e3fd7b4f5f09532df5ef4a726758f65b85833
Donnel <donnel206@hotmail.com>
2021-07-07 13:07:33 +0200

    Initial Commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore.Sqlite; 
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products {get;set;}
+
+       
+    }
+}
