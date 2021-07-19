9e7674895a26087014889cf85cd335c39023f3aa
Donnel <donnel206@hotmail.com>
2021-07-19 11:41:04 +0200

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -48,6 +48,14 @@ namespace API
 
             services.AddAplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt=>
+            {
+                opt.AddPolicy("CorsPolicy",policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+
+            });
 
         }
 
@@ -63,6 +71,7 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
             app.UseSwaggerDocumention();
