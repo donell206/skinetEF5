f14bb0dfc7a0dd568bf65a86b42f4cb1586e5741
Donnel <donnel206@hotmail.com>
2021-08-23 12:01:44 +0200

    end of validation section 16

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -10,10 +10,12 @@ namespace API.Controllers
     public class BasketController : BaseApiController
     {
         private readonly IBasketRepository _basketRepository;
-    
+        private readonly IMapper _mapper;
+
         public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
-           
+            _mapper = mapper;
+
             _basketRepository = basketRepository;
         }
 
@@ -26,11 +28,10 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
         {
-         
-
-            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            var customerBasket= _mapper.Map<CustomerBasketDto,CustomerBasket>(basket);
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
 
             return Ok(updatedBasket);
         }
